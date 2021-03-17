15fc7ecbe5ed1a91b21bc7d54e23eebb115fe2ca
Wenxi Jin <jwenxi@gmail.com>
2021-03-17 12:40:27 +0100

    Add multiple-cursors mode

--- packages.el
+++ packages.el
@@ -10,6 +10,7 @@
 ;(package! some-package)
 (package! ggtags)
 (package! nyan-mode)
+(package! multiple-cursors)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
