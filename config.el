915af66c14ec8b48859ceaa49575e777549d6af5
Wenxi Jin <jwenxi@gmail.com>
2021-03-17 13:03:01 +0100

    Add global key bindings for multiple-cursors

--- config.el
+++ config.el
@@ -87,3 +87,9 @@
 ;; Set the buffer size to 64K on Windows (from the original 4K)
 (when (boundp 'w32-pipe-buffer-size)
   (setq irony-server-w32-pipe-buffer-size (* 64 1024)))
+
+;; Multiple-cursors
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
