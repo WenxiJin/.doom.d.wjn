80edc33fd1ed85a97dd9c70554e00275de87a403
Wenxi Jin <jwenxi@gmail.com>
2021-11-12 13:01:38 +0100

    // C++: change access-label

--- config.el
+++ config.el
@@ -67,6 +67,7 @@
 ;; NOTE: Mine first, google-c-style after
 (defun my-c-mode-hook ()
   (c-set-offset 'innamespace [2])
+  (c-set-offset 'access-label '-)
   (setq c-basic-offset 2
         tab-width 2)
   (whitespace-mode 1)
